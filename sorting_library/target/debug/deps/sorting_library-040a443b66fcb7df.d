d20ce0886c26836916fddcb024c42d01d2a2a1e9
etienne <etienne.pautet@gmail.com>
2024-10-15 18:08:06 -0300

    update files

---
+++ sorting_library/target/debug/deps/sorting_library-040a443b66fcb7df.d
@@ -0,0 +1,6 @@
+C:\Users\etien\OneDrive\Área de Trabalho\Rust\sorting_library\target\debug\deps\libsorting_library-040a443b66fcb7df.rmeta: src/main.rs src\sorting_menu.rs
+
+C:\Users\etien\OneDrive\Área de Trabalho\Rust\sorting_library\target\debug\deps\sorting_library-040a443b66fcb7df.d: src/main.rs src\sorting_menu.rs
+
+src/main.rs:
+src\sorting_menu.rs:
--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,9 +1,59 @@
-use rand::seq::SliceRandom; // 0.7.2
+let mut name_list: Vec<String> = Vec::new();
 
-fn main() {
-    let entry_map = vec!["Customs", "Reserve", "Lighthouse", "Woods", "Shoreline", "Interchange", "Streets", "Labs", "Factory"]; 
-    let entry_map: Vec<_> = entry_map
-        .choose_multiple(&mut rand::thread_rng(), 1)
-        .collect();
-    println!("{:?}", entry_map);
+
+fn add_name(name_list: &mut Vec<String>) {
+
+    println!("Enter a name to add: ");
+    io::stdout().flush().unwrap();
+
+    let mut name = String::new();
+    io::stdin().read_line(&mut name).expect("Failed to read line");
+    name_list.push(name.trim().to_string());
+
+    println!("Name added!");
+
+}
+
+fn show_names(name_list: &Vec<String>) {
+
+    if name_list.is_empty() {
+        println!("The list is empty. ");
+
+    } else {
+        for (index, name) in name_list.iter().enumerate() {
+            println!("{}: {}", index, name);
+
+        }
+    }
+}
+
+fn remove_name(name_list: &mut Vec<String>) {
+    show_names(name_list);
+
+    if name_list.is_empty() {
+        return;
+
+    }
+
+    println!("Enter the index of the name to remove: ");
+    io::stdout().flush().unwrap();
+
+    let mut index_str = String::new();
+    io::stdin().read_line(&mut index_str).expect("Failed to read line");
+
+
+    let index: usize = match index_str.trim().parse() {
+        ok(i) => i,
+        Err(_) => {
+            println!("Invalid index.");
+            return;
+        }
+    };
+
+    if index < name_list.len() {
+        name_list.remove(index);
+        println!("Name removed!");
+    } else {
+        println!("Index out of bounds.");
+    }
 }
